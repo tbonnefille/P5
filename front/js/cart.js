a62e47a3e672294c8081f4dc2da377bd11b8978a
tbonnefille <bonnefille.thomas@gmail.com>
2022-08-01 17:42:05 +0200

    Corrections sur cart.js et  amélioration de deleteItem

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,21 +3,33 @@ document.title = "Kanap |  Votre panier";
 
 //tableau des sous totaux
 let tableauDesSSTT = [];
-console.log(tableauDesSSTT)
 
 //récupération des données "product"
 let lsCart = localStorage.product;
-let panier = JSON.parse(lsCart);
-console.log(panier);
-console.log(panier.length);
+if (lsCart == null) {
+    // JSON.parse a besoin d'une string en paramètre
+    lsCart = '[]';
+}
+let cart = JSON.parse(lsCart);
+console.log(cart);
+console.log("lenght of cart: " + cart.length);
+// Signaler qu'il n'y a rien dans le panier 
+if (cart.length == 0) {
+    const empty_section = document.getElementById("cart__items");
+    const empty_cart = document.createElement("h2")
+    empty_cart.innerHTML = "Votre panier est vide";
+    empty_section.appendChild(empty_cart)
 
+} else {
+    document.getElementById("cart__items").innerHTML = "";
+}
 // Pour chaque produit dans le panier
-for (i = 0; i < panier.length; i++) {
-    let id = panier[i].id;
-    let color = panier[i].option;
-    let qté = panier[i].quantité;
+for (let i = 0; i < cart.length; i++) {
+    let id = cart[i].id;
+    let color = cart[i].option;
+    let qty = cart[i].quantité;
 
-    //récupérer les données complémentaires de "selection" via fetch pour éviter de saturer localhost
+    //récupérer les données complémentaires de "selection" via fetch
     fetch("http://localhost:3000/api/products/" + id)
 
         .then(function (res) {
@@ -25,206 +37,184 @@ for (i = 0; i < panier.length; i++) {
                 return res.json();
             }
         })
-
-        //traitement des données productdata pour créer les balises
+        /* .catch(function (err) {
+             console.error("intervention de catch: il y a une erreur")
+     
+         })*/
+        // Traitement des données productdata pour créer les balises
         .then(productdata => {
 
-            if (panier.length > 0) {
-                //si le panier n'est pas vide creation des cart article
+            // Si le panier n'est pas vide : creation des cart articles
+            const section = document.getElementById("cart__items");
+            const article = document.createElement("article");
+            article.classList.add("cart__item");
+            article.setAttribute('data-id', id);
+            article.setAttribute('data-color', color);
+            section.appendChild(article);
 
-                const section = document.getElementById("cart__items");
-                const article = document.createElement("article")
-                article.classList.add("cart__item");
+            const div_img = document.createElement("div");
+            div_img.classList.add("cart__item__img");
+            article.appendChild(div_img);
 
-                article.setAttribute('data-id', id);
-                article.setAttribute('data-color', color);
+            const img = document.createElement("img");
+            img.src = productdata.imageUrl;
+            img.alt = productdata.altTxt;
+            div_img.appendChild(img);
 
-                section.appendChild(article)
+            const div_content = document.createElement("div");
+            div_content.classList.add("cart__item__content");
+            article.appendChild(div_content);
 
-                const div_img = document.createElement("div");
-                div_img.classList.add("cart__item__img")
-                article.appendChild(div_img)
+            const div_description = document.createElement("div");
+            div_description.classList.add("cart__item__content");
+            div_content.appendChild(div_description);
 
-                const img = document.createElement("img");
-                img.src = productdata.imageUrl;
-                img.alt = productdata.altTxt;
-                div_img.appendChild(img)
+            const h2 = document.createElement("h2");
+            h2.innerHTML = productdata.name;
+            div_description.appendChild(h2);
 
-                const div_content = document.createElement("div");
-                div_content.classList.add("cart__item__content");
-                article.appendChild(div_content)
+            const p_color = document.createElement("p");
+            p_color.innerHTML = color;
+            h2.appendChild(p_color);
 
-                const div_description = document.createElement("div");
-                div_description.classList.add("cart__item__content");
-                div_content.appendChild(div_description)
+            const p_price = document.createElement("p");
+            p_price.innerHTML = productdata.price + "€";
+            h2.appendChild(p_price);
 
-                const h2 = document.createElement("h2");
-                h2.innerHTML = productdata.name;
-                div_description.appendChild(h2)
+            const div_settings = document.createElement("div");
+            div_description.classList.add("cart__item__content__settings");
+            div_content.appendChild(div_settings);
 
-                const p_color = document.createElement("p")
-                p_color.innerHTML = color;
-                h2.appendChild(p_color)
+            const div_quantity = document.createElement("div");
+            div_description.classList.add("cart__item__content__settings__quantity");
+            div_settings.appendChild(div_quantity);
 
-                const p_price = document.createElement("p")
-                p_price.innerHTML = productdata.price + "€";
-                h2.appendChild(p_price)
+            const p_qty = document.createElement("p");
+            p_qty.innerHTML = "Qté : " + qty;
+            div_quantity.appendChild(p_qty)
 
-                const div_settings = document.createElement("div");
-                div_description.classList.add("cart__item__content__settings");
-                div_content.appendChild(div_settings)
+            const inputQty = document.createElement('input');
+            inputQty.setAttribute('type', 'number');
+            inputQty.classList.add('itemQuantity');
+            inputQty.name = "itemQuantity";
+            inputQty.min = 1;
+            inputQty.max = 100;
+            inputQty.value = qty;
+            div_quantity.appendChild(inputQty);
 
-                const div_quantity = document.createElement("div");
-                div_description.classList.add("cart__item__content__settings__quantity");
-                div_settings.appendChild(div_quantity)
+            let itemDelete = document.createElement('div');
+            itemDelete.classList.add('cart__item__content__settings__delete');
+            div_quantity.appendChild(itemDelete);
 
-                const p_qté = document.createElement("p");
-                p_qté.innerHTML = "Qté : " + qté;
-                div_quantity.appendChild(p_qté)
+            let p_itemDelete = document.createElement('p');
+            p_itemDelete.classList.add('deleteItem');
+            p_itemDelete.innerHTML = 'Supprimer';
+            itemDelete.appendChild(p_itemDelete);
 
-                const inputQty = document.createElement('input');
-                inputQty.setAttribute('type', 'number');
-                inputQty.classList.add('itemQuantity');
-                inputQty.name = "itemQuantity";
-                inputQty.min = 1;
-                inputQty.max = 100;
-                inputQty.value = qté;
-                div_quantity.appendChild(inputQty);
+            // Calcul du sous total ds articles
+            let sousTotal = inputQty.value * productdata.price;
+            tableauDesSSTT.push(sousTotal);
+            console.log(tableauDesSSTT)
 
-                let itemDelete = document.createElement('div');
-                itemDelete.classList.add('cart__item__content__settings__delete');
-                div_quantity.appendChild(itemDelete);
-
-                let p_itemDelete = document.createElement('p');
-                p_itemDelete.classList.add('deleteItem');
-                p_itemDelete.innerHTML = 'Supprimer';
-                itemDelete.appendChild(p_itemDelete);
-
-                let sousTotal = inputQty.value * productdata.price;
-                //oninput
-                inputQty.oninput = function () {
-                    p_qté.innerHTML = "Qté : " + inputQty.value;
-                    sstt.innerHTML = "sous total : " + inputQty.value * productdata.price + "€";
-                };
-
-                tableauDesSSTT.push(sousTotal);
-                console.log(tableauDesSSTT)
-
-                const reducer = (accumulator, currentValue) => accumulator + currentValue;
-                const prixTotal = tableauDesSSTT.reduce(reducer, 0);
-                console.log(prixTotal)
-
-                let sumSSTT = document.getElementById('totalPrice');
-                sumSSTT.innerHTML = prixTotal;
-
-                //affichage du sous total
-                const sstt = document.createElement("h3")
-                sstt.innerHTML = "sous total : " + inputQty.value * productdata.price + "€";
-                div_quantity.appendChild(sstt);
-            }
-
-            else {
-
-                const empty_section = document.getElementById("cart__items");
-                const empty_cart = document.createElement("h2")
-                empty_cart.innerHTML = "Le panier est vide";
-                empty_section.appendChild(empty_cart)
-
-            }
-
-            //fonctions
-            modifyQty();
-            calcQty();
-
-            //Calcul de la quantité globale et du prix
-            function calcQty() {
-                let eltQuantity = document.getElementsByClassName('itemQuantity');
-
-                let totalQuantitySelect = 0;
-
-                for (let q = 0; q < eltQuantity.length; q++) {
-
-                    totalQuantitySelect += parseInt(eltQuantity[q].value);
+            // Gestion du changement de quantité dû à l'input(listen)
+            inputQty.addEventListener("input", () => {
+                newQty = parseInt(inputQty.value);
+                if (newQty <= 0 || newQty > 100) {
+                    alert("Veuillez sélectionner une quantité comprise entre 1 et 100");
+                } else {
+                    // Changer le panier
+                    cart[i].quantité = newQty;
+                    // Calculer et afficher le prix de la ligne d'article
+                    sstt.innerHTML = "sous total : " + newQty * productdata.price + "€";
+                    // Changer le sous-total dans le tableau des ss-totaux
+                    tableauDesSSTT[i] = newQty * productdata.price;
+                    // Mettre l'affichage de la quantité de l'article à jour
+                    p_qty.innerHTML = "Qté : " + inputQty.value;
+                    // Calculer et afficher le prix total
+                    calcQty();
+                    // Mettre à jour le localStorage
+                    saveCartToLocalStorage();
                 }
-                let totalQuantityItems = document.getElementById('totalQuantity');
-                totalQuantityItems.innerHTML = totalQuantitySelect;
-                /*   
-                    //calcul du Prix
-                    let totalPrice = 0;
-                    for (let p = 0; p < eltQuantity.length; p++) {
-                        totalPrice += parseInt(eltQuantity[p].value) * productdata[p].price;
-                    }
-                    let productTotalPrice = document.getElementById('totalPrice');
-                    productTotalPrice.innerHTML = totalPrice;
-                    */
-            }
+            });
 
-            //modifier  les quantités
-            function modifyQty() {
-                const modifQty = document.querySelectorAll('.itemQuantity');
+            // Calcul du prix total via reducer(somme des sous totaux) et affichage
+            const reducer = (accumulator, currentValue) => accumulator + currentValue;
+            const prixTotal = tableauDesSSTT.reduce(reducer, 0);
+            console.log(prixTotal)
+            let sumSSTT = document.getElementById('totalPrice');
+            sumSSTT.innerHTML = prixTotal;
+            //affichage du sous total par article
+            const sstt = document.createElement("h3")
+            sstt.innerHTML = "sous total : " + inputQty.value * productdata.price + "€";
+            div_quantity.appendChild(sstt);
 
-                for (let m = 0; m < modifQty.length; m++) {
-                    modifQty[m].addEventListener('change', function (e) {
-                        e.preventDefault();
-
-                        panier[m].quantité = e.target.value;
-
-                        if (
-                            panier[m].quantité == 0 ||
-                            panier[m].quantité > 100
-                        ) {
-                            alert("Veuillez sélectionner une quantité comprise entre 1 et 100");
-                        }
-                        else {
-                            calcQty();
-                            localStorage.product = JSON.stringify(panier);
-                            location.reload();
-
-                        }
-                    });
-                }
-            }
-
-            //delete item
-
+            // Suppression d'article
             let deleteItem = document.querySelectorAll(".deleteItem");
-
             console.log(deleteItem)
 
-            for (let d = 0; d < deleteItem.length; d++) {
-                deleteItem[d].addEventListener(("click"), (e) => {
-                    e.preventDefault();
+            p_itemDelete.addEventListener("click", function () {
+                // On passe la quantité à 0 pour l'élémpent courant
+                cart[i].quantité = 0;
+                // On filtre le panier sur les éléments ayant au moins 1 en quantité
+                cart = cart.filter(elt => elt.quantité > 0);
+                // On sauvegarde le panier
+                saveCartToLocalStorage();
+                // On recharge la page
+                location.reload();
+            });
+            // On recalcule les quantités
+            calcQty();
 
-                    // avec la méthode filter je selectionne les éléments à garder et je supprime l'élément clické
-
-                    let idDelItem = panier[d].id;
-                    let colorDelItem = panier[d].option;
-
-
-                    panier = panier.filter(
-                        (element) => element.id == idDelItem && element.option !== colorDelItem || element.option !== colorDelItem
-
-                    );
-                    console.log(panier)
-                    localStorage.setItem('product', JSON.stringify(panier));
-                    //location.reload();
-                })
-
-
-            }
         })
-    /*
-    
-            .catch(function (err) {
-                console.error("intervention de catch: il y a une erreur")
-    
-            })
-    
-    */
+
+    //Calcul de la quantité globale et du prix total
+    function calcQty() {
+        let eltQuantity = document.getElementsByClassName('itemQuantity');
+        let totalQuantitySelect = 0;
+
+        for (let elt of eltQuantity) {
+            totalQuantitySelect += parseInt(elt.value);
+        }
+        let totalQuantityItems = document.getElementById('totalQuantity');
+        totalQuantityItems.innerHTML = totalQuantitySelect;
+
+        // Calcul et affichage du prix total
+        let totalPrice = tableauDesSSTT.reduce((acc, current) => acc + current, 0);
+        document.getElementById("totalPrice").innerHTML = totalPrice;
+
+    }
+
+    // Modifier les quantités des articles
+    function modifyQty(evt, canap, option) {
+        const modifQty = evt.target;
+
+        for (let m = 0; m < modifQty.length; m++) {
+            modifQty[m].addEventListener('change', function (e) {
+                e.preventDefault();
+
+                cart[m].quantité = e.target.value;
+
+                if (
+                    cart[m].quantité == 0 ||
+                    cart[m].quantité > 100
+                ) {
+                    alert("Veuillez sélectionner une quantité comprise entre 1 et 100");
+                } else {
+                    calcQty();
+                    saveCartToLocalStorage();
+                }
+
+            });
+        }
+    }
+    // Fonction de MAJ du ls
+    function saveCartToLocalStorage() {
+        localStorage.product = JSON.stringify(cart);
+
+    }
 
 }
 
-
-
+//////////////////////////////Gestion du formulaire
 
 
--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,4 @@
-//récupération de id dans l'URL via urlParams.get
+// Récupération de id dans l'URL via urlParams.get
 let urlParams = new URLSearchParams(location.search);
 let URLid = urlParams.get('id');
 
@@ -13,9 +13,9 @@ fetch('http://localhost:3000/api/products/' + URLid)
   })
 
   .then(data => {
-    //renomage de la page en fonction du produit
+    // Renomage de la page en fonction du produit
     document.title = data.name;
-    //création du contenu de l'article descriptif du produit
+    // Création du contenu de l'article descriptif du produit
     const div = document.getElementsByClassName("item__img")[0];
     const img = document.createElement("img");
     img.src = data.imageUrl;
@@ -29,7 +29,7 @@ fetch('http://localhost:3000/api/products/' + URLid)
     const description = document.getElementById("description");
     description.innerHTML = data.description;
 
-    //renseignement des options de couleur
+    // Renseignement des options de couleur
     for (let color of data.colors) {
       const select = document.getElementById('colors');
       const option = document.createElement("option");
@@ -39,75 +39,72 @@ fetch('http://localhost:3000/api/products/' + URLid)
 
     }
 
-    //oninput pour calcul du sous otal en temps réel
+    // Oninput pour calcul du sous total en temps réel
     let inputQty = document.querySelector("input");
     inputQty.oninput = function () {
       sstt.innerHTML = "sous total : " + inputQty.value * data.price + "€";
     };
 
-    //sous total
+    // Affichage du sous total
     const div_icsq = document.getElementsByClassName("item__content__settings__quantity")[0];
-    const sstt = document.createElement("h2")
+    const sstt = document.createElement("h2");
     sstt.innerHTML = " Sous total : " + data.price * document.getElementById("quantity").value + "€";
     div_icsq.appendChild(sstt);
 
     //////////////////////////////////////////////addToCart///////////////////////////////////////////////////////////////
 
-
     const addToCart_btn = document.getElementById("addToCart");
 
-    //listen
+    // Validation listen
     addToCart_btn.addEventListener('click', (e) => {
       (e).preventDefault();
 
-      //récupérer les données de couleur et qte
+      // Récupérer les données de couleur et qte
       let selectedColor = document.getElementById("colors").value;
       //let qte = document.getElementById("quantity").value; ATTENTION LE PIEGE!
-      let qte = parseInt(document.getElementById("quantity").value);
+      let qty = parseInt(document.getElementById("quantity").value);
 
-      //obliger le visiteur à choisir des quantité et couleurs valides
+      // Obliger le visiteur à choisir des quantité et couleurs valides
       if (
-        qte == 0 ||
-        qte > 100 ||
+        qty == 0 ||
+        qty > 100 ||
         selectedColor == null ||
         selectedColor == ''
-      )
-       {
+      ) {
         alert("Veuillez selectionner une couleur et une quantité comprise entre 1 et 100");
         return;
       };
 
-      //canapé choisi
+      // Canapé choisi
       let selection = {
         id: data._id,
         option: selectedColor,
-        quantité: qte
+        quantité: qty
 
       }
-      console.log(selection)
+      console.log(selection);
 
       ///////////////////////////////////////////////////////gestion du localstorage/////////////////////////////////////////////
 
-      
       let lsCart = localStorage.getItem("product");
-
+      // S"assurer que lsCart exist bien (sous forme de string)
       if (!lsCart) {
         lsCart = '[]';
       }
-      let panier = JSON.parse(lsCart);
-      console.log(panier)
+      let cart = JSON.parse(lsCart);
+      console.log(cart)
       // Créer un drapeau "trouvé" à false
-      let foundFlag = false
+      let foundFlag = false;
       // Parcourir le panier => for
-      for (let product of panier) {
+      for (let product of cart) {
         // À chaque objet :
         // Vérifier si l'objet correspond à la sélection
         // Si c'est le cas (if)
         if (product.id === selection.id && product.option === selection.option) {
-          //    je lève le drapeau (trouvé = true)
-          foundFlag = true
-          //    Je modifie la quantité dans la panier
-          product.quantité += qte
+          // Je lève le drapeau (trouvé = true)
+          foundFlag = true;
+          // Je modifie la quantité dans la panier
+          product.quantité += qty;
         }
         // Sinon (else) je passe à l'objet suivant
       }
@@ -115,19 +112,17 @@ fetch('http://localhost:3000/api/products/' + URLid)
       // Si le drapeau n'est pas levé (false) c'est que je n'ai pas l'objet dans le panier
       if (foundFlag == false) {
         // => je l'ajoute
-        panier.push(selection);
+        cart.push(selection);
       }
       // Dans tous les cas on remet le panier dans le storage
-      localStorage.product = JSON.stringify(panier);
-      
-
-
+      localStorage.product = JSON.stringify(cart);
 
     })
-    /*
-      })
-      .catch(function (err) {
-        console.error("intervention de catch: il y a une erreur")
-    */
+
+  })
+
+  .catch(function (err) {
+    console.error("intervention de catch: il y a une erreur")
+
   })
 
